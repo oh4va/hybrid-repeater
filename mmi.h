bee957345cf713bf56d80d433cf01edfd42565c2
Veijo Arponen <veijo.arponen@arpotechno.fi>
2019-07-07 12:49:57 +0300

    MMI version V1.0.0

---
+++ mmi.h
@@ -0,0 +1,100 @@
+/** @file mmi.h
+    @brief Type definitions and global variables
+
+    This contains the type definitions of the mixed mode repeater
+    controller and eventually any macros, constants,
+    or global variables you will need.
+
+    @author Veijo Arponen, OH4VA <vepa@iki.fi>
+    @bug No known bugs.
+*/
+
+
+#define VERSION "V1.0.0"
+
+/** Supported boards */
+#if defined(__AVR_ATmega168__) || defined(__AVR_ATmega328P__)
+#define ARDUINO_UNO
+
+#elif defined(__AVR_ATtiny85__)
+#define ARDUINO_ATTINY
+
+#else
+#error "Unsupported board selected!"
+#endif
+
+/** Pin mappings for ARDUINO UNO */
+#ifdef ARDUINO_UNO
+#define GPIO_SVXLINK_PIN      2   // output
+#define GPIO_MMDVM_PIN        3   // output
+#define GPIO_PTT_PIN          4   // output
+#define GPIO_DMKURI_PTT_PIN   5   // input
+#define GPIO_MMDVM_PTT_PIN    6   // input
+#endif
+
+/** Pin mappings for ATtiny85 */
+#ifdef ARDUINO_ATTINY
+#define GPIO_SVXLINK_PIN      0   // output
+#define GPIO_MMDVM_PIN        1   // output
+#define GPIO_PTT_PIN          2   // output
+#define GPIO_DMKURI_PTT_PIN   3   // input
+#define GPIO_MMDVM_PTT_PIN    4   // input
+#endif
+
+/** Delay times */
+#ifdef ARDUINO_UNO
+#define TIME_START      0
+#define TIME_LOOP       2000
+#define TIME_LOOP_TOT   2000
+#else
+#define TIME_START      3000
+#define TIME_LOOP       10
+#define TIME_LOOP_TOT   100
+#endif
+
+/** How long in seconds the last mode is kept selected if mode hang is active */
+#define MODE_HANG_TIME 20
+
+/** Minimum transmit time in seconds after which mode hang is activated */
+#define MODE_HANG_THRESHOLD 8
+
+/** Maximum transmit time (TOT) in seconds*/
+#define PTT_ON_MAX 3600
+
+/** FSM states */
+#define MMI_IDLE    0
+#define SVX_TX      1
+#define SVX_HANG    2
+#define DVM_TX      3
+#define DVM_HANG    4
+
+/** Type definition for the MMI machine */
+typedef struct {
+  byte  Out;       // FSM output
+  byte  Next[40];  // Next state
+} fsm_t;
+
+
+/** FSM output bits */
+#define OUT_NON_BIT  B000  // All output bits are OFF
+#define OUT_PTT_BIT  B100  // TX PTT ON
+#define OUT_SVX_BIT  B001  // Select TX signal from SVXLINK
+#define OUT_DVM_BIT  B010  // Select TX signal from MMDVM
+
+/** Indexed transition table with output and delay time */
+const fsm_t FSM[] = {
+  //    Combined GPIO output, FSM input byte values:                                                               //
+  //                           00000000, 00000001, 00000010, 00000011, 00000100, 00000101, 00000110,  00000111,    //
+  {              OUT_NON_BIT, {MMI_IDLE,   SVX_TX,   DVM_TX,   SVX_TX, MMI_IDLE,   SVX_TX,   DVM_TX,    SVX_TX}},  // State 0 IDLE
+  {OUT_PTT_BIT | OUT_SVX_BIT, {MMI_IDLE,   SVX_TX, MMI_IDLE,   SVX_TX, SVX_HANG,   SVX_TX, SVX_HANG,    SVX_TX}},  // State 1 SVX TX
+  {              OUT_SVX_BIT, {MMI_IDLE, MMI_IDLE, MMI_IDLE,   SVX_TX, SVX_HANG,   SVX_TX, SVX_HANG,    SVX_TX}},  // State 2 SVX HANG
+  {OUT_PTT_BIT | OUT_DVM_BIT, {MMI_IDLE, MMI_IDLE,   DVM_TX,   DVM_TX, DVM_HANG, DVM_HANG,   DVM_TX,    DVM_TX}},  // State 3 DVM TX
+  {              OUT_DVM_BIT, {MMI_IDLE, MMI_IDLE,   DVM_TX,   DVM_TX, DVM_HANG, DVM_HANG,   DVM_TX,    DVM_TX}}   // State 4 DVM HANG
+};
+
+/** Global variables */
+byte state = MMI_IDLE;              // FSM current state
+unsigned long ptt_on_timer = 0;     // TX ON timer
+unsigned long mode_hang_timer = 0;  // Mode hang countdown timer
+byte mode_hang_on = 0;              // Mode hang timer is ON
+byte ptt_tot_on = 0;                // TX ON max time exceeded
---
+++ mmi.ino
@@ -0,0 +1,197 @@
+/** @file mmi.ino
+    @brief
+
+    This file contains the main functionality
+    of the mixed mode repeater interface code.
+
+    @author Veijo Arponen, OH4VA <vepa@iki.fi>
+    @bug No known bugs.
+*/
+
+#include <avr/io.h>
+#include <avr/pgmspace.h>
+
+#include "mmi.h"
+
+/**
+  Sets up the Arduino board.
+*/
+void setup() {
+
+  SetPinModes();
+
+  // Glow ANA/DIGI Leds for three seconds
+  digitalWrite(GPIO_SVXLINK_PIN, HIGH);
+  digitalWrite(GPIO_MMDVM_PIN, HIGH);
+  delay(TIME_START);
+
+#ifdef ARDUINO_UNO
+  Serial.begin(9600);
+  Serial.print("Multi-Mode Interface ");
+  Serial.print(VERSION);
+  Serial.print('\n');
+#endif
+}
+
+/**
+  Actively controls the Arduino board.
+*/
+void loop() {
+
+  byte input = 0;           // FSM input
+  byte output = 0;          // FSM output
+  unsigned long wait_time;  // FSM state delay
+
+#ifdef ARDUINO_UNO
+  Serial.print("State: ");
+  Serial.print(state);
+  Serial.print(", ");
+#endif
+
+  // Reset TOT ON flag
+  if (state == MMI_IDLE) {
+    ptt_tot_on = 0;
+  }
+
+  // Select state output and delay time
+  output = FSM[state].Out;
+
+  // Set output, toggle only if TX has been ON too long
+  SetOutput(output, ptt_tot_on);
+
+#ifdef ARDUINO_UNO
+  Serial.print("Out: ");
+  Serial.print(output, BIN);
+  Serial.print(", ");
+
+  Serial.print("TOT: ");
+  Serial.print(ptt_tot_on);
+  Serial.print(", ");
+#endif
+
+  // Set wait time
+  wait_time = ptt_tot_on ? TIME_LOOP_TOT : TIME_LOOP;
+
+  // Wait
+  delay(wait_time);
+
+  // Increment PTT-ON timer if TX is ON
+  if (output & OUT_PTT_BIT) {
+    ptt_on_timer++;
+  }
+  else {
+    ptt_on_timer = 0;
+  }
+
+  // Decrement mode hang timer
+  if (mode_hang_timer > 0 ) {
+    mode_hang_timer--;
+  }
+
+  // Set mode hang timer
+  if (ptt_on_timer > MODE_HANG_THRESHOLD * 1000UL / TIME_LOOP) {
+    mode_hang_timer = MODE_HANG_TIME * 1000UL / TIME_LOOP;
+  }
+
+  // Set mode hang ON
+  if (mode_hang_timer > 0 && !ptt_tot_on) {
+    mode_hang_on = 1;
+  }
+  else {
+    mode_hang_on = 0;
+  }
+
+  // Set TOT ON flag if TX has been ON too long
+  if (ptt_on_timer > PTT_ON_MAX * 1000UL / TIME_LOOP) {
+    ptt_tot_on = 1;
+  }
+
+  // Read inputs
+  input = ReadInput();
+
+#ifdef ARDUINO_UNO
+  Serial.print("Input: ");
+  Serial.print(input, BIN);
+  Serial.print('\n');
+#endif
+
+  // Select next state based on the input byte
+  state = FSM[state].Next[input];
+}
+
+
+/**
+  Set digital pin modes (input/output)
+
+  @param none
+  @return none
+*/
+void SetPinModes() {
+
+  // Digital inputs
+  pinMode(GPIO_DMKURI_PTT_PIN, INPUT_PULLUP);
+  pinMode(GPIO_MMDVM_PTT_PIN, INPUT_PULLUP);
+
+  // Digital outputs
+  pinMode(GPIO_SVXLINK_PIN, OUTPUT);
+  pinMode(GPIO_MMDVM_PIN, OUTPUT);
+  pinMode(GPIO_PTT_PIN, OUTPUT);
+}
+
+/**
+  Read input
+
+  @param none
+  @return Input value for the FSM packed in one unsigned char variable
+*/
+
+byte ReadInput() {
+
+  byte input = 0;
+
+  // Copy mode hang state to the FSM input byte
+  input |= mode_hang_on;
+  input = input << 1;
+
+  // Read PTT state from MMDVM
+  input |= (!digitalRead(GPIO_MMDVM_PTT_PIN));
+  input = input << 1;
+
+  // Read PTT state from DMK Engineering URI or RB RIM Lite
+  input |= (!digitalRead(GPIO_DMKURI_PTT_PIN));
+
+  return input;
+}
+
+/**
+  Write output to GPIO
+
+  @param output Output bits
+  @param toggle Toggle LEDs on every call
+  @return none
+*/
+
+inline void SetOutput(byte output, bool toggle) {
+
+  // Only toggle LEDs
+  if (toggle) {
+    // Set PTT OFF
+    digitalWrite(GPIO_PTT_PIN, LOW);
+    // Blink SVXLINK LED
+    if (output & OUT_SVX_BIT) {
+      digitalWrite(GPIO_SVXLINK_PIN, !digitalRead(GPIO_SVXLINK_PIN));
+    }
+    // Blink MMDVM LED
+    if (output & OUT_DVM_BIT) {
+      digitalWrite(GPIO_MMDVM_PIN, !digitalRead(GPIO_MMDVM_PIN));
+    }
+  }
+  else {
+    // Set SVXLINK ON/OFF
+    digitalWrite(GPIO_SVXLINK_PIN, output & OUT_SVX_BIT);
+    // Set MMDVM ON/OFF
+    digitalWrite(GPIO_MMDVM_PIN, output & OUT_DVM_BIT);
+    // Set PTT ON/OFF
+    digitalWrite(GPIO_PTT_PIN, output & OUT_PTT_BIT);
+  }
+}
